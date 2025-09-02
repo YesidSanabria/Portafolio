2019b1bfc03a67b38b55b480400d90b60dbba345
Yesid Sanabria <yesanabria2@poligran.edu.co>
2025-09-02 09:32:47 -0500

    Nombre

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mi-portafolio-juego-base' title`, () => {
+  it(`should have the 'mi-portafolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mi-portafolio-juego-base');
+    expect(app.title).toEqual('mi-portafolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mi-portafolio-juego-base');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mi-portafolio');
   });
 });
